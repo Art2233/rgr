68107f4fff9c34186b497b9740b126168f302f47
abiluba9 <abiluba9@gmail.com>
2025-12-20 01:34:46 +0200

    refactor: remove TestReq component and associated routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
-import { TestReq } from './test-req/test-req';
-import { MainPage } from './main-page/main-page';
 import { RailwayContainerComponent } from './railway-page/railway-container/railway-container.component';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => MainPage,
-    },
-    {
-        path: 'test-req',
-        loadComponent: () => TestReq,
-    },
-    {
-        path: 'railway',
         loadComponent: () => RailwayContainerComponent,
-    }
+    },
 ];
